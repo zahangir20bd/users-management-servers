79b71a701c55b72f4a68ea95d756fee860f220bb
Zahangir Alam Rafe <rafe.z@standard-group.com>
2023-05-09 16:53:24 +0600

    code added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: 1, name: "Rafiq Ahmed", email: "rafiq@mail.com" },
@@ -20,7 +21,14 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
+  console.log("Post API is hitting");
   console.log(req.body);
+  const newUser = req.body;
+
+  newUser.id = users.length + 1;
+  users.push(newUser);
+
+  res.send(newUser);
 });
 
 app.listen(port, () => {
